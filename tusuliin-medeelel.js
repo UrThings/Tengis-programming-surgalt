69c11738ceaa9d7b133b1a36a0607cad7ed1746e
UrThings <160115501+UrThings@users.noreply.github.com>
2024-03-11 15:32:50 +0800

    Add files via upload

---
+++ tusuliin-medeelel.js
@@ -0,0 +1,29 @@
+
+export const tusuluud = [{
+  name: '1р төсөл',
+  title: 'Дэлгэрэнгүй if нөхцөл шалгах үйлдэл - жижиг програм',
+  image: "OIP.jpg",
+  number: '101'
+},{
+  name: '2р төсөл',
+  title: 'Жижиг чатын програм хийх',
+  image: "OIP.jpg",
+  number: '102'
+},{
+  name: '3р төсөл',
+  title: 'Онгоц бууддаг тоглоом',
+  image: "OIP.jpg",
+  number: '103'
+},{
+  name: '4р төсөл',
+  title: 'Толь бичгийн програм зохиох',
+  image: "OIP.jpg",
+  number: '104'
+},{
+  name: '5р төсөл',
+  title: 'ATM програм хийцгээе',
+  image: "OIP.jpg",
+  number: '105'
+}];
+
+
